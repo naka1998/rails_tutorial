3e3cbe6c0ab3d0763d997e0fa531b03b4ec4b594
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-09-25 10:43:32 +0900

    fixed

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-orkers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
